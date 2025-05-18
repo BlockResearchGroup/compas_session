8523b94b9377dc273ba4e0b53017ad41e3544160
tomvanmele <tom.v.mele@gmail.com>
2025-05-18 19:22:54 +0200

    clean up package data

--- src/compas_session/__init__.py
+++ src/compas_session/__init__.py
@@ -1,19 +1,1 @@
-import os
-
-
-__author__ = ["tom van mele"]
-__copyright__ = "ETH Zurich - Block Research Group"
-__license__ = "MIT License"
-__email__ = "tom.v.mele@gmail.com"
 __version__ = "0.4.5"
-
-
-HERE = os.path.dirname(__file__)
-
-HOME = os.path.abspath(os.path.join(HERE, "../../"))
-DATA = os.path.abspath(os.path.join(HOME, "data"))
-DOCS = os.path.abspath(os.path.join(HOME, "docs"))
-TEMP = os.path.abspath(os.path.join(HOME, "temp"))
-
-
-__all__ = ["HOME", "DATA", "DOCS", "TEMP"]
