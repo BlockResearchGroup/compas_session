a87e4f42e2ae34e923d61f875e4d9f27245b831f
tomvanmele <tom.v.mele@gmail.com>
2025-05-18 19:23:07 +0200

    add lazy load test

---
+++ tests/test_lazy.py
@@ -0,0 +1,58 @@
+import pytest
+
+from compas_session.lazyload import LazyLoadSession, LazyLoadSessionError
+
+
+def test_session_noname():
+    with pytest.raises(TypeError):
+        LazyLoadSession()
+
+
+def test_session_name_empty():
+    with pytest.raises(LazyLoadSessionError):
+        LazyLoadSession(name=None)
+    with pytest.raises(LazyLoadSessionError):
+        LazyLoadSession(name="")
+
+
+def test_session_singleton():
+    session1a = LazyLoadSession(name="One")
+    session1b = LazyLoadSession(name="One")
+    session2a = LazyLoadSession(name="Two")
+    session2b = LazyLoadSession(name="Two")
+
+    assert session1a is session1b
+    assert session2a is session2b
+
+    assert session1a is not session2a
+    assert session1a is not session2b
+    assert session1b is not session2a
+    assert session1b is not session2b
+
+
+def test_session_settings():
+    session1a = LazyLoadSession(name="One")
+    session1b = LazyLoadSession(name="One")
+    session2a = LazyLoadSession(name="Two")
+    session2b = LazyLoadSession(name="Two")
+
+    assert session1a.settings is session1b.settings
+    assert session2a.settings is session2b.settings
+
+    assert session1a.settings is not session2a.settings
+    assert session1a.settings is not session2b.settings
+    assert session1b.settings is not session2a.settings
+    assert session1b.settings is not session2b.settings
+
+
+def test_session_settings_values():
+    session1 = LazyLoadSession(name="One")
+    session2 = LazyLoadSession(name="Two")
+
+    assert session1.settings.autosave is True
+    assert session2.settings.autosave is True
+
+    session1.settings.autosave = False
+
+    assert session1.settings.autosave is False
+    assert session2.settings.autosave is True
